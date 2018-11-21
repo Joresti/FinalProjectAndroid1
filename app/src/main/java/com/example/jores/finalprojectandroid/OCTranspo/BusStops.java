01dd9aeb443f8fcdb43abe74fdd1981f5ae0c7cf
Chad <chadsouster@hotmail.com>
2018-11-21 13:49:53 -0500

    First commit before Nick kills my code

---
+++ app/src/main/java/com/example/jores/finalprojectandroid/OCTranspo/BusStops.java
@@ -0,0 +1,14 @@
+package com.example.jores.finalprojectandroid.OCTranspo;
+import com.example.jores.finalprojectandroid.R;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BusStops extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_stops);
+    }
+}
--- app/src/main/java/com/example/jores/finalprojectandroid/StartActivity.java
+++ app/src/main/java/com/example/jores/finalprojectandroid/StartActivity.java
@@ -4,12 +4,16 @@ import android.app.Activity;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.jores.finalprojectandroid.CBCNews.CBCNewsMain;
+import com.example.jores.finalprojectandroid.OCTranspo.OCTranspoMain;
 
 public class StartActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME = "StartActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +22,26 @@ public class StartActivity extends Activity {
 
         Button cbcBtn =  findViewById(R.id.cbcNews);
         Button foodBtn = findViewById(R.id.foodNutrition);
-        Button movieBtn =  findViewById(R.id.movies);
+        Button ocpBtn =  findViewById(R.id.ocTranspoApp);
+
+
 
         cbcBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(StartActivity.this, CBCNewsMain.class);
                 startActivity(i);
+                Log.i(ACTIVITY_NAME, "Switching to CBCNewsMain");
 
+            }
+        });
 
+        ocpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(StartActivity.this, OCTranspoMain.class);
+                startActivity(i);
+                Log.i(ACTIVITY_NAME, "Switching to OCTranspoMain");
             }
         });
 
